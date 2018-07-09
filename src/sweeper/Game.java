aa236c0c5d90570ae6584603f93f29c7a6c04a91
airenma <airenma@gmail.com>
2018-07-09 12:58:31 +0300

    new

--- src/sweeper/Game.java
+++ src/sweeper/Game.java
@@ -3,20 +3,26 @@ package sweeper;
 public class Game {
 
     private Bomb bomb;
+    private Flag flag;
 
     public Game (int cols, int rows, int bombs)
     {
         Ranges.setSize(new Coord (cols, rows));
         bomb = new Bomb(bombs);
+        flag = new Flag();
     }
 
     public void start()
     {
        bomb.start();
+       flag.start();
     }
 
     public Box getBox(Coord coord)
     {
-        return bomb.get(coord);
+        if(Box.OPENED == flag.get(coord))
+            return bomb.get(coord);
+        else
+            return flag.get(coord);
     }
 }
